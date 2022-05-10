6037da6d74278f052f2a9931cc91fb9586009ef5
mnecibi <mehdi.necibi@capgemini.com>
2022-05-10 01:10:29 +0100

    update cron job

--- config/prod.exs
+++ config/prod.exs
@@ -3,5 +3,5 @@ import Config
 config :dictio, Dictio.Scheduler,
 jobs: [
   # Runs every midnight:
-  {"@daily",  {Dictio.Database, :store_word_of_the_day, []}},
+  {"0 * * * *",  {Dictio.Database, :store_word_of_the_day, []}},
 ]
